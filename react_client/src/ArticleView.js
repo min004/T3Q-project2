595b2884c43395614bbe9dce303d1ce4a1eac851
min004 <113760511+min004@users.noreply.github.com>
2023-01-12 06:46:41 +0900

    articleview design

--- react_client/src/ArticleView.js
+++ react_client/src/ArticleView.js
@@ -2,6 +2,8 @@ import React, { Component, useEffect, useState }from "react";
 import axios from "axios";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
+
+
 export const ArticleView = (props) => {
 const articleId_temp = decodeURI(window.location.href).split('http://localhost:3000/article/').reverse()[0];
 const articleId = articleId_temp.split('/')[0] - 1;
@@ -11,27 +13,7 @@ const [title, setTitle] = useState([''])
 const [registerId, setRegisterId] = useState([''])
 const [registeDate, setRegisteDate] = useState([''])
 const [content, setContent] = useState([''])
-// const Board = ({
-//     id,
-//     title,
-//     content,
-//     registerId,
-//     registerDate,
-// }) => {
-//     return (
-//         <tbody>
-//         <tr align="center">
-//             <td width="50px">{id}</td>
-//             <td width="300px">{title}</td>
-//             <td width="50px">{registerId}</td>
-//             <td width="100px">{registerDate}</td>
-//         </tr>
-//         <tr>
-//             <td>{content}</td>
-//         </tr>
-//         </tbody>
-//     );
-// };
+
 
 useEffect(() => {
     console.log("렌더링 될때마다 실행");
@@ -67,37 +49,36 @@ useEffect(() => {
                                 <td width="500px"><b>{title}</b></td>
                                 
                             </tr>
+                            
                             <tr align="left" border-bottom='1px' padding= '10px'>
                                 <td width="50px">작성자</td>
                                 <td width="500px">{registerId}</td>
                                 {/* <td width="100px">{registeDate}</td> */}
                             </tr>
                             <tr align="left" border-bottom='1px' padding= '10px'>
+                                <td width="50px">작성일</td>
+                                <td width="500px">{registeDate}</td>
+                                {/* <td width="100px">{registeDate}</td> */}
+                            </tr>
+                            <tr>
+                                <td colSpan={2}>
+                                <div style={{height:'1px', backgroundColor:'#46536B', width:'100%'}}></div>
+                                </td>
+                            </tr>
+                            <tr align="left" border-bottom='1px' padding= '10px'>
                                 <td width="50px">내용</td>
                                 <td>{content}</td>
                             </tr>
+                            <tr>
+                                <td colSpan={2}>
+                                <div style={{height:'1px', backgroundColor:'#46536B', width:'100%'}}></div>
+                                </td>
+                            </tr>
                         </tbody>
-                            {/* {
-                                // eslint-disable-next-line
-                                Object.values(state.article).map((v) => {
-                                    // console.log(v)
-                                    return (
-                                        console.log(v),
-                                        
-                                        <Board key={v.id}
-                                            id={v.id}
-                                            title={v.subject}
-                                            registerDate={v.create_date}
-                                            registerId={v.creator}
-                                            content={v.content}
-                                            // registerDate={v.REGISTER_DATE}
-                                            // key={v.BOARD_ID}
-                                        />
-                                    );
-                                })
-                                } */}
                     </Table>
-                    <Button style={{marginLeft: '90vh'}} onClick={() => window.location.href = "/write"}>글쓰기</Button> 
+                    <Button 
+                    style={{marginLeft: '0vh'}} onClick={() => window.location.href = "/write"}>글쓰기</Button>
+                    <Button style={{marginLeft: '80%'}} onClick={() => window.location.href = "/hello"}>목록으로</Button> 
                     {/* <Button variant="secondary">수정하기</Button>
                     <Button variant="danger">삭제하기</Button>
                     <Button variant="info">글쓰기</Button> */}
